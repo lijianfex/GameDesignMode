1bd09c38b5b00f80312f3e181ca4fcee1e910ec6
lijian <1287053830@qq.com>
2018-05-26 15:26:13 +0800

    对ICharaterAttr中的公用属性提取为BaseAttr,运用享元模式，避免每个角色中都有相同属性，造成内存浪费

--- Assets/Scripts/GameSystem/CharacterSystem/Attr/ICharacterAttr.cs
+++ Assets/Scripts/GameSystem/CharacterSystem/Attr/ICharacterAttr.cs
@@ -11,19 +11,19 @@ public  class ICharacterAttr
     protected float mMoveSpeed;     //移动速度
     protected string mIConSprite;   //头像图片 
     protected string mPrefabName;   //预支名
-
-    protected int mCurrentHP;       //当前HP    
-
-    protected int mLv;              //等级（士兵的等级会改变） 敌人默认等级为1
     protected float mCriRate;       //0-1的暴击率 (敌人才有暴击率) 士兵没有
+
+    protected int mCurrentHP;       //当前HP 
+
+    protected int mLv;              //等级（士兵的等级会改变） 敌人默认等级为1   
 
     //通过等级，暴击率计算：  增加的最大血量   抵御的伤害值   暴击增加的伤害 -------这些数值的计算就要采取策略模式 
     protected IAttrStrategy mStrategy;
 
-    protected int mDmgDesValue; //抵御的伤害值    
-
-    public int CritValue { get { return mStrategy.GetCritDmg(mCriRate); } }//暴击增加的伤害
     public int CurrentHP { get { return mCurrentHP; } } //获取当前血量
+    protected int mDmgDesValue; //抵御的伤害值
+    public int CritValue { get { return mStrategy.GetCritDmg(mCriRate); } }//暴击增加的伤害
+    
 
 
     public ICharacterAttr(IAttrStrategy strategy,string name,int lv,int maxHP,float moveSpeed,string iconSprite,string prefabName)

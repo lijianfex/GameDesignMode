0bb3c4422801fd4766125a18c41f24982f3fcbcf
lijian <1287053830@qq.com>
2018-05-27 12:21:03 +0800

    简单搭建Main场景，可以跳转到Battle场景

--- Assets/Scripts/SceneState/MainState.cs
+++ Assets/Scripts/SceneState/MainState.cs
@@ -1,10 +1,32 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class MainState : ISceneState
 {
+    private Button BtnPaly;
+
     public MainState(SceneStateManager sceneStateManager) : base("02Main", sceneStateManager)
     {
 
+    }
+
+
+
+    public override void StateStart()
+    {
+        GameObject root = GameObject.Find("Login");
+        BtnPaly = UnityTool.FindChild<Button>(root, "BtnPlay").GetComponent<Button>();
+        BtnPaly.onClick.AddListener(OnPlayClick);
+    }
+
+    private void OnPlayClick()
+    {
+        mSceneStateManager.SetState(new BattleState(mSceneStateManager));
+    }
+
+    public override void StateEnd()
+    {
+        BtnPaly.onClick.RemoveAllListeners();
     }
 }

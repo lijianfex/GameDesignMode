0eff552dd322814b9468bc67f7af65e03ba68577
lijian <1287053830@qq.com>
2018-06-03 17:44:50 +0800

    责任链模式原型，用于关卡设计

---
+++ Assets/DesignModeCode/08ChainOfResponsibility/DM08ChainOfResponsibility.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 责任链模式（设计关卡）
+/// </summary>
+public class DM08ChainOfResponsibility : MonoBehaviour
+{
+    private void Start()
+    {
+        string problem = "C";
+        IDMHander handerA = new DMHanderA();
+        IDMHander handerB = new DMHanderB();
+        IDMHander handerC = new DMHanderC();
+        handerA.NextHander = handerB;
+        handerB.NextHander = handerC;
+
+        handerA.Hander(problem);
+
+    }
+}
+
+
+public abstract class IDMHander
+{
+
+    protected IDMHander mNextHander = null;
+    public IDMHander NextHander
+    {
+        set
+        {
+            mNextHander = value;
+        }
+    }
+
+    public virtual void Hander(string problem) { }
+
+
+}
+
+public class DMHanderA : IDMHander
+{
+    public override void Hander(string problem)
+    {
+        if (problem == "A")
+        {
+            Debug.Log("处理完A问题");
+        }
+        else
+        {
+            if (mNextHander != null)
+            {
+                mNextHander.Hander(problem);
+            }
+        }
+
+    }
+}
+
+
+public class DMHanderB : IDMHander
+{
+    public override void Hander(string problem)
+    {
+        if (problem == "B")
+        {
+            Debug.Log("处理完B问题");
+        }
+        else
+        {
+            if (mNextHander != null)
+            {
+                mNextHander.Hander(problem);
+            }
+        }
+    }
+}
+
+public class DMHanderC : IDMHander
+{
+    public override void Hander(string problem)
+    {
+        if (problem == "C")
+        {
+            Debug.Log("处理完C问题");
+        }
+        else
+        {
+            if (mNextHander != null)
+            {
+                mNextHander.Hander(problem);
+            }
+        }
+    }
+}

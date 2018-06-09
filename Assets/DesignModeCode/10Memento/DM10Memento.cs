a03011c1df89494485dcb406250a0e8ac971cb1f
lijian <1287053830@qq.com>
2018-06-09 22:05:45 +0800

    添加备忘录模式原型

---
+++ Assets/DesignModeCode/10Memento/DM10Memento.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 备忘录模式，用于成就系统数据的保存
+/// </summary>
+public class DM10Memento :MonoBehaviour
+{
+
+}
+
+/// <summary>
+/// 发起者
+/// </summary>
+class Originator
+{
+    private string mState;
+
+    public void SetState(string state)
+    {
+        mState = state;
+    }
+
+    public void ShowState()
+    {
+
+    }
+
+    public Memento CreatMemento()
+    {
+        Memento memento = new Memento();
+        memento.SetState(mState);
+        return memento;
+    }
+
+}
+
+/// <summary>
+/// 备忘录
+/// </summary>
+class Memento
+{
+    private string mState;
+
+    public void SetState(string state)
+    {
+        mState = state;
+    }
+
+    public string GetState()
+    {
+        return mState;
+    }
+}

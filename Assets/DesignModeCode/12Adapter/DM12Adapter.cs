e21cf4218e71c265cccea4e0b8e7357e1ef8550a
lijian <1287053830@qq.com>
2018-06-11 13:40:57 +0800

    完成适配器模式原型

---
+++ Assets/DesignModeCode/12Adapter/DM12Adapter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///适配器模式（伏兵营的应用）
+/// </summary>
+public class DM12Adapter : MonoBehaviour
+{
+    private void Start()
+    {
+        Adapter adapter = new Adapter(new NewPlugin());
+        StanderInterface s = adapter;
+        s.Request();
+    }
+}
+
+interface StanderInterface
+{
+    void Request();
+}
+
+/// <summary>
+/// 标准组件
+/// </summary>
+class StanderImpentA : StanderInterface
+{
+    public void Request()
+    {
+        Debug.Log("使用标准方法实现");
+    }
+}
+
+
+/// <summary>
+/// 适配器类
+/// </summary>
+class Adapter:StanderInterface
+{
+    private NewPlugin newPlugin;
+
+    public Adapter(NewPlugin p)
+    {
+        newPlugin = p;
+    }
+
+    public void Request()
+    {
+        newPlugin.SpecificRequest();
+    }
+}
+
+
+/// <summary>
+/// 新的插件
+/// </summary>
+class NewPlugin
+{
+    public void SpecificRequest()
+    {
+        Debug.Log("使用特殊方法实现");
+    }
+}
